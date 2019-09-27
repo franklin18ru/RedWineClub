97e3398e12ae73f54c963bc2190abc75dd74aeaf
franklin18 <franklin18@ru.is>
2019-09-27 17:47:23 +0000

    Changed favicon

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
-
 const logo = process.env.PUBLIC_URL + '/rwclogo.png';
+const profileImg = process.env.PUBLIC_URL + '/avatar-placeholder.png';
+class App extends Component {
+  render() {
+    return (
+        <div className={'App-header App'}>
+          <div className={'row'}>
+            <div className={'col'}>
+              <img src={logo} alt="Logo" id={'logo'}/>
+            </div>
+            <div className={'col'}>
+              <img src={profileImg} alt="profileImg"/>
+            </div>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Pussy
-        </p>
-        <a
-          className="App-link"
-          href="https://pornhub.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Sex
-        </a>
-      </header>
-    </div>
-  );
+          </div>
+        </div>
+    );
+  }
 }
 
 export default App;
+
